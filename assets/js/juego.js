bac62a3d4a9db3ca98bcc007326d9d0f506d7f8a
Jorge Piqueras <jorgepiqueras92@gmail.com>
2023-12-14 16:33:04 +0100

    feat: resto de funciones

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,8 +6,14 @@ let puntosJugador = 0;
 let puntosComputadora = 0;
 const puntosHTML = document.querySelectorAll('small');
 //referencias Html
+let victoriasJugador
 const divCartasJugador = document.querySelector('#jugador-cartas')
+const divCartasComputadora = document.querySelector('#computador-cartas')
+
 const btnPedir = document.querySelector('#btnPedir')
+const btnParar = document.querySelector('#btnParar')
+const btnNuevo = document.querySelector('#btnNuevo')
+
 
 const crearDeck = () => {
     for(let i = 2; i <= 10;i++) {
@@ -48,6 +54,25 @@ const valorCarta = (carta ) => {
     return (isNaN(valor))?(valor === 'A') ? 11 : 10 : valor * 1 ;
     
 }
+//turno de la computadora
+
+const turnoComputadora = (puntosMinimos) => {
+    do{
+        
+        const carta = pedirCarta();
+    puntosComputadora = puntosComputadora +  valorCarta(carta);
+
+    
+    const imgCarta = document.createElement('img')
+    imgCarta.src = `assets/cartas/${carta}.png `
+    imgCarta.classList.add('carta')
+    puntosHTML[1].innerText = puntosComputadora;
+    divCartasComputadora.append(imgCarta)
+    }
+    while(( puntosComputadora <= puntosMinimos ) && ( puntosMinimos <= 21 ));
+    
+}
+
 
 // Eventos
 btnPedir.addEventListener('click', () => {
@@ -63,10 +88,38 @@ btnPedir.addEventListener('click', () => {
     if (puntosJugador > 21) {
         console.warn('lo siento perdiste')
         btnPedir.disabled = true;
+        btnParar.disabled = true;
+        turnoComputadora(1)
     } else if (puntosJugador === 21){
         console.log('Genial ganaste')
         btnPedir.disabled = true;
+        btnParar.disabled = true;
+        turnoComputadora(puntosJugador)
     }
 
 
 });
+
+btnParar.addEventListener('click', () => {
+    if (puntosJugador <= 21) {
+        turnoComputadora(puntosJugador);
+        
+    }
+    btnPedir.disabled = true;
+    btnParar.disabled = true;
+    
+
+});
+btnNuevo.addEventListener('click', () => {
+    puntosHTML[0].innerText = 0;
+    puntosHTML[1].innerText = 0;
+    puntosComputadora = 0
+    puntosJugador = 0
+    crearDeck();
+    divCartasJugador.innerHTML = '';
+    divCartasComputadora.innerHTML = '';
+    btnPedir.disabled = false;
+    btnParar.disabled = false;
+
+});
+
